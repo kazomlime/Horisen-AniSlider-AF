cce3a2f5a17beb6b6df97eecb61fb74a1c367dc2
Emil Stojanovic <emil@horisen.com>
2019-05-29 15:16:59 +0200

    premium-sms-transit added

---
+++ js/premium-sms-transit/premium-sms-transit.js
@@ -0,0 +1,7 @@
+var animation = lottie.loadAnimation({
+    container: document.getElementById('anislider'),
+    renderer:'svg',
+    loop: true,
+    autoplay: true,
+    path: 'js/premium-sms-transit/premium-sms-transit.json'
+})
